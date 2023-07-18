a46443c294d1cc9f778d6337462a4136526e9b48
zhansultan <jansultan_141096@mail.ru>
2023-07-18 17:27:11 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import LayoutHeader from "@/layout/Header.vue";
+import LayoutSidebar from "@/layout/Sidebar.vue";
+import { ref } from "vue";
+
+const isOpenMenu = ref(false);
+
+const toggleMenu = () => {
+  isOpenMenu.value = !isOpenMenu.value;
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="sidebar-toggle" @click="toggleMenu">&#5125;</div>
+    <layout-header />
+    <layout-sidebar :openSidebar="isOpenMenu" />
+    <div :class="['content', { content_full: !isOpenMenu }]">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.content {
+  max-width: 1400px;
+  margin-left: 250px;
+  padding: 30px;
+  transition: 0.2s;
+  &_full {
+    margin-left: 0;
+  }
+}
+
+.sidebar-toggle {
+  position: fixed;
+  left: 0;
+  width: 15px;
+  background: var(--primary);
+  height: 100%;
+  top: 62px;
+  z-index: 1;
+  cursor: pointer;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media screen and (max-width: 1023px) {
+  .content {
+    margin-left: 0;
+  }
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Uikit Vue3+Vite</h1>
+</template>
+
+<style lang="scss" scoped>
+h1{
+  color: var(--primary)
+}
+</style>
